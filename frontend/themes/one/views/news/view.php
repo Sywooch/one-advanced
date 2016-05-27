768c9c16cd08335ec17e02d14cac362bd49a774f
Ivan Kuzmich <kusma007@inbox.ru>
2016-05-27 15:22:09 +0200

    fix news view

--- frontend/themes/one/views/news/view.php
+++ frontend/themes/one/views/news/view.php
@@ -13,13 +13,13 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1 class=""><?= Html::encode($this->title) ?></h1>
     <?php
     echo Html::tag('div',Html::tag('span',Yii::$app->formatter->asDate($model -> date,'dd.MM.yy'),['class'=>'news-date']).Html::tag('span',$model->category->name,['class'=>'news-category']),['class'=>'date-category news-index-date']);
-
     $images = $model->getImages();
     if($images[0]['urlAlias']!='placeHolder' && $images[0]->isMain) {
         $image = $model->getImage();
-        $sizes = $image->getSizesWhen('x200');
+        $sizes = $image->getSizesWhen('400x');
         //            echo Html::img($image->getUrl('x300'),['class' => 'center-block img-responsive','width'=>$sizes['width'], 'height'=>$sizes['height']]);
         echo Html::img($image->getUrl('x200'),['class' => 'news-view-img thumbnail img-responsive','width'=>$sizes['width']]);
     }
-    echo Html::tag('div',$model->content,['class'=>'']); ?>
+
+    echo Html::tag('div',$model->content,['class'=>'news-view-content']); ?>
 </div>
