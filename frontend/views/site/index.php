d20c7339334bc5d9fd1a0d0fb6c5c51097ed9a4d
kusma007 <kusma007@inbox.ru>
2016-06-10 00:55:15 +0200

    добавил youtube канал

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -4,6 +4,8 @@ use frontend\widgets\NewsWidget;
 use yii\widgets\ListView;
 use yii\helpers\Html;
 use yii\helpers\Url;
+use Madcoda\Youtube;
+use Madcoda\Tests\YoutubeTest;
 
 /* @var $this yii\web\View */
 
@@ -36,52 +38,52 @@ $this->title = 'Сайт Футбольного Клуба';
             </div>
         </div>
     </div>
-
-
-    <?php // echo NewsWidget::widget(['template'=>'news-index']); ?>
-
-    <!--<div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>-->
-
-    <!--<div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+    <div class="video-tv">
+        <h3>Канал-ТВ</h3>
+        <div class="well">
+            <div class="row">
+                <?php
+                $key='AIzaSyAvDmtfH6P73IJzaV4bN0JyoJl--3Z4tc8';
+                $youtube = new Youtube(array('key' => $key));
+                $channel = $youtube->getChannelByName('fcbaltika');
+                $playlist = $channel->contentDetails->relatedPlaylists->uploads;
+                $playlistItems = $youtube->getPlaylistItemsByPlaylistId($playlist);
+                $i = 0;
+                foreach ($playlistItems as $item) {
+            //        var_dump($item);
+                    $i++;
+                    if ($i <= 3) {
+                        if ($i==1){
+                            ?>
+                            <div class="col-xs-9">
+                                <div class="video-left-block">
+                                    <iframe type='text/html' src='http://www.youtube.com/embed/<?php echo $item->snippet->resourceId->videoId ?>?showinfo=0' width='100%' height='330' frameborder='0' allowfullscreen></iframe>
+                                    <div class="video-name"><?php echo $item->snippet->title ?></div>
+                                </div>
+                            </div>
+                            <?php
+                        } elseif ($i==2) {
+                           ?>
+                            <div class="col-xs-3">
+                                <div class="video-right-block">
+                                    <div class="video-right-block-top">
+                                        <iframe type='text/html' src='http://www.youtube.com/embed/<?php echo $item->snippet->resourceId->videoId ?>?showinfo=0' width='100%' height='100' frameborder='0' allowfullscreen></iframe>
+                                        <div class="video-name"><?php echo $item->snippet->title ?></div>
+                                    </div>
+                            <?php
+                        } elseif ($i==3) {
+                            ?>
+                                    <div class="video-right-block-bottom">
+                                        <iframe type='text/html' src='http://www.youtube.com/embed/<?php echo $item->snippet->resourceId->videoId ?>?showinfo=0' width='100%' height='100' frameborder='0' allowfullscreen></iframe>
+                                        <div class="video-name"><?php echo $item->snippet->title ?></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php
+                        }
+                    }
+                }
+                    ?>
             </div>
         </div>
-
-    </div>-->
-</div>
+    </div>
