1166766d253e5d78ac612dc90ed95aab30bed96c
kusma007 <kusma007@inbox.ru>
2016-08-02 00:05:28 +0200

    small fix in news

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -84,7 +84,7 @@ class SiteController extends Controller
         $this->layout = 'main-index';
 
         $dataProvider['news'] = new ActiveDataProvider([
-            'query' => News::find()->where(['status_id'=>'on'])->orderBy('date_create ASC')->limit(10),
+            'query' => News::find()->where(['status_id'=>'on'])->orderBy('date_create DESC')->limit(10),
             'pagination' => [
                 'pageSize' => 7,
             ],
