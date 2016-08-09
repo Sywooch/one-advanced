ab95b69237d3154bdda44774157b6d715697067f
Ivan Kuzmich <kusma007@inbox.ru>
2016-08-09 18:17:24 +0200

    Merge branches 'master' and 'one-dev' of github.com:kusma007/one-advanced into one-dev
    Conflicts:
    	frontend/widgets/views/gallery-index.php

--- frontend/themes/one/widgets/views/gallery-index.php
+++ frontend/themes/one/widgets/views/gallery-index.php
@@ -11,31 +11,27 @@ foreach ($model as $item) {
         <div class="col-xs-12">
             <h5><a href="<?php echo Url::to(['/gallery/view', 'id' => $item->id]); ?>"><?php echo $item->name ?></a></h5>
             <div class="gallery-view-block">
-            <?php
+                <?php
                 $images = $item->getImages();
                 if($images[0]['urlAlias']!='placeHolder') {
                     $i = 0;
                     foreach($images as $img){
-                        $class = ['target' => '_blank'];
-                        $imgExtension = pathinfo($img->filePath)['extension'];
-                        if ($imgExtension != '') {
-                            $class = ['class' => 'lightbox'];
-                        }
                         $i++;
-                        if ($i == 4) {
+                        if ($i == 5) {
                             break;
                         }
                         echo Html::tag(
                             'div',
                             Html::a(
-                                Html::img($img->getUrl('100x100'),['alt' => $item->name, 'class' => 'gallery-home-img']),
-                                $img->getUrl(),$class
+                                Html::img($img->getUrl('100x100'),['alt' => $item->name, 'class' => 'gallery-home-img img-responsive']),
+                                $img->getUrl(),
+                                ['target' => '_blank']
                             ),
                             ['class' => 'gallery-view-box']
                         );
                     }
                 }
-            ?>
+                ?>
             </div>
         </div>
     </div>
