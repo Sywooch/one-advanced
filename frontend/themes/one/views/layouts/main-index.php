797d6147181fa3b63ba4f73ad30aa89774fa2928
kusma007 <kusma007@inbox.ru>
2016-09-01 00:44:38 +0200

    add yandex

--- frontend/themes/one/views/layouts/main-index.php
+++ frontend/themes/one/views/layouts/main-index.php
@@ -996,6 +996,19 @@ $this->title = $this->title . ' | ФК ' . Yii::$app->params['main-team'];
                             ?>
                             <div class="copyright">Разработка и поддержка - <a href="http://pixlet.ru/">Pixlet</a></div>
                         </div>
+                        <div class="pull-right">
+                            <!--LiveInternet counter--><script type="text/javascript"><!—
+                                document.write("<a href='//www.liveinternet.ru/click' "+
+                                    "target=_blank><img src='//counter.yadro.ru/hit?t17.1;r"+
+                                    escape(document.referrer)+((typeof(screen)=="undefined")?"":
+                                    ";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+                                        screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+                                    ";"+Math.random()+
+                                    "' alt='' title='LiveInternet: показано число просмотров за 24"+
+                                    " часа, посетителей за 24 часа и за сегодня' "+
+                                    "border='0' width='88' height='31'><\/a>")
+                                //--></script><!--/LiveInternet-->
+                        </div>
                     </div>
                     <!--            </div>-->
                 </div>
