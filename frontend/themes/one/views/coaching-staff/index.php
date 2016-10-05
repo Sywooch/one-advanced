c7e77ff57f75b23a005f1238af35289899be9379
kusma007 <kusma007@inbox.ru>
2016-10-06 00:15:56 +0200

    add category caches and update caches

--- frontend/themes/one/views/coaching-staff/index.php
+++ frontend/themes/one/views/coaching-staff/index.php
@@ -12,22 +12,42 @@ $this->title = $title;
 ?>
 <div class="coaching-staff-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<!--    <h1>--><?php // echo Html::encode($this->title) ?><!--</h1>-->
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
 <?php Pjax::begin(); ?>
-    <?php echo ListView::widget([
-        'dataProvider' => $dataProvider,
-        'options' => [
-            'class' => 'row',
-        ],
-        'itemOptions' => ['class' => 'item col-xs-4'],
-        'itemView' => '_list',
-        'summary' => false,
-        //
-        //        'itemView' => function ($model, $key, $index, $widget) {
-        //            return Html::a(Html::encode($model->name), ['view', 'id' => $model->id]);
-        //        },
-    ]);
+    <?php
+    foreach($dataProvider as $key => $item) {
+        if($key != null) {
+            echo Html::beginTag('div', ['class' => 'list-view']);
+                echo Html::tag('h3', $data['allCategory'][$key]);
+                echo ListView::widget([
+                    'dataProvider' => $item,
+                    'options' => [
+                        'class' => 'row',
+                    ],
+                    'itemOptions' => ['class' => 'item col-xs-4'],
+                    'itemView' => '_list',
+                    'summary' => false,
+                ]);
+            echo Html::endTag('div');
+        }
+    }
+    if (isset($dataProvider[null])) {
+        echo ListView::widget([
+            'dataProvider' => $dataProvider[null],
+            'options' => [
+                'class' => 'row',
+            ],
+            'itemOptions' => ['class' => 'item col-xs-4'],
+            'itemView' => '_list',
+            'summary' => false,
+            //
+            //        'itemView' => function ($model, $key, $index, $widget) {
+            //            return Html::a(Html::encode($model->name), ['view', 'id' => $model->id]);
+            //        },
+        ]);
+    }
+
     ?>
 <?php Pjax::end(); ?></div>
