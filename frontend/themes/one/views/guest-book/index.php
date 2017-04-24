0cc23b4c7dce8260845fd31cd92ea8eeba32f8e9
Ivan Kuzmich <kusma007@inbox.ru>
2017-04-24 12:42:07 +0200

    add button to login and signup in guest book

--- frontend/themes/one/views/guest-book/index.php
+++ frontend/themes/one/views/guest-book/index.php
@@ -21,6 +21,14 @@ $this->params['image_page'] = '/themes/one/src/layout/guest.png';
         echo $this->render('_form',[
             'model' => $model,
         ]);
+    } else {
+        ?>
+        <p>Для общения на гостевой Вы должны быть зарегистрирвоаны на нашем сайте!</p>
+        <p>
+            <a href="/site/login" class="btn btn-primary">Вход</a>
+            <a href="/site/signup" class="btn btn-primary">Регистрация</a>
+        </p>
+        <?php
     }
     ?>
 <?php Pjax::begin(['id' => 'guest_records']); ?>
